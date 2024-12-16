e85e10b684102a8c3a9bca7f8003f6e62e777783
Luan Nguyen Duy <33739713+duyluann@users.noreply.github.com>
2024-12-16 11:19:26 +0000

    git commit -m "feat: add created_at field to todo"

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -5,4 +5,4 @@ export const todo = pgTable('todo', {
     text: text('text').notNull(),
     done: boolean('done').default(false).notNull(),
     created_at: timestamp('created_at').notNull().defaultNow(),
-});
+  });
